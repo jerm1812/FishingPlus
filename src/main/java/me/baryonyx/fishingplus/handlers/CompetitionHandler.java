274716945e575fc0a38d92746a38f255171abda5
Jerm1812 <jeremyadventure@gmail.com>
2020-10-11 17:12:25 -0600

    Added error handling for item persistent data.

--- src/main/java/me/baryonyx/fishingplus/handlers/CompetitionHandler.java
+++ src/main/java/me/baryonyx/fishingplus/handlers/CompetitionHandler.java
@@ -1,6 +1,19 @@
 package me.baryonyx.fishingplus.handlers;
 
-public class CompetitionHandler {
+import me.baryonyx.fishingplus.FishingPlus;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class CompetitionHandler extends BukkitRunnable {
+    private final FishingPlus plugin;
     public boolean running = false;
+
+    public CompetitionHandler(FishingPlus plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void run() {
+
+    }
     //TODO Implement a competition handler
 }
