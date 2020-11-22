dc0ff05c7290004763660cc51e9bf645b58b7830
jerm1812 <jeremy@cookbunch.net>
2020-11-22 12:16:14 -0700

    Added custome model data and fixed competition prize error (#3)
    Added the ability to add custome model data.
    Fixed the issue of throwing an error for a competition prize when one was not defined for the placement of a player

--- src/main/java/me/baryonyx/fishingplus/fishing/Fish.java
+++ src/main/java/me/baryonyx/fishingplus/fishing/Fish.java
@@ -8,8 +8,8 @@ public class Fish extends Reward {
     public double actualLength;
 
     // A reward that is a fish
-    public Fish(String name, double chance, List<String> commands, double minLength, double maxLength) {
-        super(name, chance, commands);
+    public Fish(String name, double chance, int customModelData, List<String> commands, double minLength, double maxLength) {
+        super(name, chance, customModelData, commands);
         this.minLength = minLength;
         this.maxLength = maxLength;
     }
